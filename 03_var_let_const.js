b508a4150fa45c332576723e4b0539fa17e76846
krushna rathod <krushna.rathod@simformsolutions.com>
2025-03-05 10:40:09 +0530

    add file which represent working var let and const in different scope

---
+++ 03_var_let_const.js
@@ -0,0 +1,26 @@
+var a = 20;
+let b = 200;
+const c = 2000;
+
+console.log(a)
+
+{
+    var a = 10;
+    let b = 100;
+    const c = 1000;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+function hello() {
+    var a = 30;
+    console.log(a);
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+hello();
